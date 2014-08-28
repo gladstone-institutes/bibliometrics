d0563fb32de6cba94c9b73715752636e590c8439
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2014-08-27 17:48:54 -0700

    Refactored pubmed

--- src/pubmed.py
+++ src/pubmed.py
@@ -1,10 +1,11 @@
 from io import BytesIO
 import re
+from itertools import count
 import requests
 import requests_cache
 import lxml.etree
 import lxml.html
-import ref
+from util import has_keys, xpath_str, xpath_strs
 
 def _split_range(n, m):
   low = 0
@@ -13,174 +14,140 @@ def _split_range(n, m):
     yield (low, high)
     low = high
 
-def _xmlget(url, params):
-  req = requests.get(url, params=params)
-  content = BytesIO(req.content)
-  parser = lxml.etree.XMLParser(recover=True, encoding='utf-8')
-  return lxml.etree.parse(content, parser)
-
-def _htmlget(url, params):
-  req = requests.get(url, params=params)
-  parser = lxml.html.HTMLParser(recover=True, encoding='utf-8')
-  return lxml.html.document_fromstring(req.content, parser=parser)
-
-
-def _make_citmatch_str(refs, lo, hi):
-  citmatch_str = u''
-  for i in range(lo, hi):
-    ref = refs[i]
-    if hasattr(ref, 'journal') and hasattr(ref, 'year') and hasattr(ref, 'volume') and hasattr(ref, 'firstpage') and hasattr(ref, 'authors_str'):
-      citmatch_str += (ref.journal     + '|' + \
-                       ref.year        + '|' + \
-                       ref.volume      + '|' + \
-                       ref.firstpage   + '|' + \
-                       ref.authors_str + '|' + \
-                       str(i)          + '|' + \
-                       '%0D\n')
-  return citmatch_str
-
 def _ref_to_citmatch_str(ref, refkey):
   return ref['journal']       + '|' + \
          ref['year']          + '|' + \
          ref['volume']        + '|' + \
          ref['firstpage']     + '|' + \
          ref['authors'][0][0] + '|' + \
-         refkey
+         refkey               + '|' + \
+         '%0D'
 
 _pmid_re = re.compile(r'\d+')
-def _pmids_by_citmatch(refs, lo, hi):
-  citmatch_str = _make_citmatch_str(refs, lo, hi)
-  if not citmatch_str:
-    return
-  req = requests.get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/ecitmatch.cgi',
-          params={'db': 'pubmed', 'retmode': 'xml', 'bdata': citmatch_str})
-  pmids_raw = req.text
-  pmid_lines = pmids_raw.split('\n')
-  for pmid_line in pmid_lines:
-    pmid_line = pmid_line.strip()
-    if not pmid_line: continue
-    pieces = pmid_line.split('|')
-    pmid = pieces[-1].encode('utf-8')
-    index = pieces[-2]
-    index = int(index)
-    if _pmid_re.match(pmid):
-      refs[index].pmid = pmid
 
-def _make_esearch_term(ref):
-  if ref.authors_str == 'None':
-    return u'({title}[Title])'.format(title=ref.title)
+def _ref_to_esearch_term(ref):
+  title = ref['title']
+  if not 'authors' in ref or not ref['authors']:
+    return u'({title}[Title])'.format(title=title)
   else:
-    author = ref.first_author()
-    return u'({title} [Title]) AND ({author} [Author - First])'.format(title=ref.title, author=author)
-
-def _pmid_by_author_title_search(ref):
-  esearch_term = _make_esearch_term(ref)
-  tree = _xmlget('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi', params={'db': 'pubmed', 'term': esearch_term})
-  ids = tree.xpath('/eSearchResult/IdList/Id/text()')
-  if ids and len(ids) == 1:
-    ref.pmid = ids[0].encode('utf-8')
-
-def _pmid_by_author_title_scrape(ref):
-  esearch_term = _make_esearch_term(ref)
-  tree = _htmlget('http://www.ncbi.nlm.nih.gov/pubmed/', params={'term': esearch_term})
-  idtag = tree.cssselect('.abstract .aux .rprtid .highlight')
-  if not idtag == []:
-    ref.pmid = idtag[0].text.encode('utf-8')
+    author = ref['authors'][0][0]
+    return u'({title} [Title]) AND ({author} [Author - First])'.format(title=title, author=author)
 
 class Client:
   def __init__(self):
-    requests_cache.install_cache('.req-cache')
+    self.session = requests_cache.CachedSession('.req-cache')
+    self.xml_parser = lxml.etree.XMLParser(recover=True, encoding='utf-8')
+    self.html_parser = lxml.html.HTMLParser(recover=True, encoding='utf-8')
 
-  def add_pmids(self, refs):
+  def _add_pmids_by_citmatch(self, refs):
+    searchable_refs = [ref for ref in refs if not 'pmid' in ref and has_keys(ref, 'journal', 'year', 'volume', 'firstpage', 'authors')]
+    if not searchable_refs:
+      return
+
+    citmatch_str = '\n'.join([_ref_to_citmatch_str(ref, str(i)) for (ref, i) in zip(searchable_refs, count())])
+
+    req = self.session.get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/ecitmatch.cgi',
+        params={'db': 'pubmed', 'retmode': 'xml', 'bdata': citmatch_str})
+    pmids_raw = req.text
+    
+    pmid_lines = pmids_raw.split('\n')
+    for pmid_line in pmid_lines:
+      pmid_line = pmid_line.strip()
+      if not pmid_line: continue
+      pieces = pmid_line.split('|')
+      pmid = pieces[-1].encode('utf-8')
+      index = pieces[-2]
+      index = int(index)
+      if _pmid_re.match(pmid):
+        searchable_refs[index]['pmid'] = pmid
+
+  def _add_pmid_by_author_title_scrape(self, ref):
+    esearch_term = _ref_to_esearch_term(ref)
+    req = self.session.get('http://www.ncbi.nlm.nih.gov/pubmed/', params={'term': esearch_term})
+    doc = lxml.html.document_fromstring(req.content, parser=self.html_parser)
+    idtag = doc.cssselect('.abstract .aux .rprtid .highlight')
+    if not idtag == []:
+      ref['pmid'] = idtag[0].text.encode('utf-8')
+
+  def _add_pmids(self, refs):
     for (lo, hi) in _split_range(50, len(refs)):
-      _pmids_by_citmatch(refs, lo, hi)
+      self._add_pmids_by_citmatch(refs[lo:hi])
 
     for ref in refs:
-      if not hasattr(ref, 'pmid'):
-        _pmid_by_author_title_scrape(ref)
+      if not 'pmid' in ref:
+        self._add_pmid_by_author_title_scrape(ref)
 
-  def add_pubmed_data(self, refs):
-    pmids = [ref.pmid for ref in refs if hasattr(ref, 'pmid')]
-    pmids_str = ','.join(pmids)
-    doc = _xmlget('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi',
-                     params={'db': 'pubmed', 'id': pmids_str, 'rettype': 'xml'})
+  def add_pubmed_data(self, refs, overwrite_keys = None):
+    self._add_pmids(refs)
+
+    pmids_str = ','.join((ref['pmid'] for ref in refs if 'pmid' in ref))
+    req = self.session.get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi',
+        params={'db': 'pubmed', 'id': pmids_str, 'rettype': 'xml'})
+
+    doc = lxml.etree.parse(BytesIO(req.content), self.xml_parser)
     articles = doc.xpath('/PubmedArticleSet/PubmedArticle')
     for article in articles:
-      pubmed_ref = PubMedRef(article)
-      for ref in refs:
-        if hasattr(ref, 'pmid') and ref.pmid == pubmed_ref.pmid:
-          ref.pubmed = pubmed_ref
-
-
-_pm_months = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec'
-]
-
-class PubMedRef(ref.Ref):
-  def __init__(self, article):
-    self.pmid = article.xpath('PubmedData/ArticleIdList/ArticleId[@IdType=\'pubmed\']/text()')[0]
-
-    self.authors = []
-    for author in article.xpath('MedlineCitation/Article/AuthorList/Author'):
-      lastname = author.xpath('LastName/text()')
-      initials = author.xpath('Initials/text()')
-      if lastname and initials:
-        name = unicode(lastname[0] + u' ' + initials[0])
+      pubmed_ref = _article_to_pubmed_ref(article)
+      ref = _dict_with_value(refs, 'pmid', pubmed_ref['pmid'])
+      if overwrite_keys:
+        ref.update({k: pubmed_ref.get(k, None) for k in overwrite_keys})
       else:
-        continue
-      affiliation = author.xpath('Affiliation/text()')
-      affiliation = unicode(affiliation[0]) if affiliation else None
-      self.authors.append((name, affiliation))
-    
-    self.title = article.xpath('MedlineCitation/Article/ArticleTitle/text()')[0]
+        ref.update(pubmed_ref)
 
-    pubdate_str = u''
-    #pubdate_elem = article.xpath('MedlineCitation/Article/Journal/JournalIssue/PubDate')[0]
-    pubdate_elem = article.xpath('PubmedData/History/PubMedPubDate[@PubStatus="pubmed"]')[0]
-    pubdate_yr = pubdate_elem.xpath('Year/text()')
-    if pubdate_yr:
-      pubdate_str += pubdate_yr[0]
-      pubdate_mon = pubdate_elem.xpath('Month/text()')
-      if pubdate_mon:
-        pubdate_str += '%02d' % int(pubdate_mon[0])
-        pubdate_day = pubdate_elem.xpath('Day/text()')
-        if pubdate_day:
-          pubdate_str += '%02d' % int(pubdate_day[0])
-        else:
-          pubdate_str += '00'
+def _dict_with_value(ds, k, v):
+  for d in ds:
+    if k in d and d[k] == v:
+      return d
+  return None
+
+def _article_to_pubmed_ref(article):
+  r = {}
+  r['pmid'] = xpath_str(article, 'PubmedData/ArticleIdList/ArticleId[@IdType=\'pubmed\']/text()')
+
+  institutions = {}
+  authors = []
+  for author in article.xpath('MedlineCitation/Article/AuthorList/Author'):
+    lastname = xpath_str(author, 'LastName/text()')
+    initials = xpath_str(author, 'Initials/text()')
+    if lastname and initials:
+      name = lastname + u' ' + initials
+    else:
+      continue
+    institution_address = xpath_str(author, 'Affiliation/text()')
+    institution_index = len(institutions) + 1 if institution_address else None
+    if institution_address:
+      institutions[institution_index] = (institution_address, None)
+    authors.append((name, institution_index))
+  r['authors'] = authors
+  r['institutions'] = institutions
+  
+  r['title'] = xpath_str(article, 'MedlineCitation/Article/ArticleTitle/text()')
+
+  pubdate_str = u''
+  pubdate_elem = article.xpath('PubmedData/History/PubMedPubDate[@PubStatus="pubmed"]')[0]
+  pubdate_yr = xpath_str(pubdate_elem, 'Year/text()')
+  if pubdate_yr:
+    pubdate_str += pubdate_yr
+    pubdate_mon = xpath_str(pubdate_elem, 'Month/text()')
+    if pubdate_mon:
+      pubdate_str += '%02d' % int(pubdate_mon)
+      pubdate_day = xpath_str(pubdate_elem, 'Day/text()')
+      if pubdate_day:
+        pubdate_str += '%02d' % int(pubdate_day)
       else:
-        pubdate_str += '0000'
+        pubdate_str += '00'
+    else:
+      pubdate_str += '0000'
 
-    self.pubdate = int(pubdate_str) if pubdate_str else None
-    self.year = pubdate_yr[0] if pubdate_yr else None
+  r['pubdate'] = int(pubdate_str) if pubdate_str else None
+  r['year'] = pubdate_yr
+  r['journal'] = xpath_str(article, 'MedlineCitation/MedlineJournalInfo/MedlineTA/text()')
+  r['grantagencies'] = xpath_strs(article, 'MedlineCitation/Article/GrantList[last()]/Grant/Agency/text()')
+  r['pubtypes'] = xpath_strs(article, 'MedlineCitation/Article/PublicationTypeList/PublicationType/text()')
 
-    journal = article.xpath('MedlineCitation/MedlineJournalInfo/MedlineTA/text()')
-    self.journal = journal[0] if journal else None
-
-    self.grantagencies = map(unicode, article.xpath('MedlineCitation/Article/GrantList[last()]/Grant/Agency/text()'))
-
-    self.pubtypes = map(unicode, article.xpath('MedlineCitation/Article/PublicationTypeList/PublicationType/text()'))
-
-    self.allterms = []
-    for meshheading in article.xpath('MedlineCitation/MeshHeadingList/MeshHeading'):
-      terms = meshheading.xpath('DescriptorName/text() | QualifierName/text()')
-      self.allterms.append(terms)
-
-    citreq = requests.get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi',
-        params={'db': 'pubmed', 'linkname': 'pubmed_pubmed_citedin', 'id': self.pmid})
-    citdoc = lxml.etree.fromstring(citreq.content)
-    self.citcount = len(citdoc.xpath('/eLinkResult/LinkSet/LinkSetDb/Link'))
-
-  def first_author(self):
-    return self.authors[0][0]
+  allterms = []
+  for meshheading in article.xpath('MedlineCitation/MeshHeadingList/MeshHeading'):
+    terms = xpath_str(meshheading, 'DescriptorName/text() | QualifierName/text()')
+    allterms.append(terms)
+  r['meshterms'] = allterms
+  return r
--- src/util.py
+++ src/util.py
@@ -1,7 +1,13 @@
-def xpath_str(doc, path, ns):
+def xpath_str(doc, path, ns = None):
   result = doc.xpath(path, namespaces=ns)
   return unicode(result[0]) if result else None
 
-def xpath_strs(doc, path, ns):
+def xpath_strs(doc, path, ns = None):
   results = doc.xpath(path, namespaces=ns)
   return map(unicode, results)
+
+def has_keys(d, *keys):
+  for key in keys:
+    if not key in d:
+      return False
+  return True
