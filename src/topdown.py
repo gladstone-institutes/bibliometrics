faf65ae30dd010461cf388ee661c85b4028cd9cc
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2014-08-28 18:44:16 -0700

    Made top down a class

--- src/topdown.py
+++ src/topdown.py
@@ -5,59 +5,62 @@ import clinicaltrials
 import pubmed
 import wos
 
-def _first_author(ref):
-  authors = ref.get('authors')
-  if not authors:
-    return None
-  else:
-    return authors[0][0]
+class TopDown:
+  def __init__(self):
+    self.net = litnet.LitNet()
+    self.pm_client = pubmed.Client()
+    self.ct_client = clinicaltrials.Client()
+    self.wos_client = wos.Client()
 
-def _main(input_file_path):
-  net = litnet.LitNet()
-  pm_client = pubmed.Client()
-  ct_client = clinicaltrials.Client()
-  wos_client = wos.Client()
+  def _first_author(self, ref):
+    authors = ref.get('authors')
+    if not authors:
+      return None
+    else:
+      return authors[0][0]
 
-  input_file = open(input_file_path, 'r')
-  input_lines = input_file.readlines()
-  input_file.close()
+  def run(self, input_file_path):
+    input_file = open(input_file_path, 'r')
+    input_lines = input_file.readlines()
+    input_file.close()
 
-  drug_name = input_lines[0].strip()
-  drug_index = net.add_v(type='drug', label=drug_name)
+    drug_name = input_lines[0].strip()
+    drug_index = self.net.add_v(type='drug', label=drug_name)
 
-  trials = ct_client.search(drug_name)
-  for trial in trials:
-    trial_index = net.add_v(type='clinicaltrial', title=trial['title'], label=trial['nctid'])
-    net.g.add_edge(drug_index, trial_index)
+    trials = self.ct_client.search(drug_name)
+    for trial in trials:
+      trial_index = self.net.add_v(type='clinicaltrial', title=trial['title'], label=trial['nctid'])
+      self.net.g.add_edge(drug_index, trial_index)
 
-    refs = trial['biblio']
-    if refs:
-      pm_client.add_pubmed_data(refs)
+      refs = trial['biblio']
+      if refs:
+        self.pm_client.add_pubmed_data(refs)
 
-      for ref in refs:
-        wos_refs = wos_client.search(_first_author(ref), ref.get('title'), ref.get('journal'), ref.get('year'))
-        if len(wos_refs) == 1:
-          ref.update(wos_refs[0])
+        for ref in refs:
+          wos_refs = self.wos_client.search(self._first_author(ref), ref.get('title'), ref.get('journal'), ref.get('year'))
+          if len(wos_refs) == 1:
+            ref.update(wos_refs[0])
 
-      for ref in refs:
-        net.add_ref(ref, trial_index)
+        for ref in refs:
+          self.net.add_ref(ref, trial_index)
 
-  fda_index = net.add_v(type='clinicaltrial', label='FDA')
-  fda_refs = refparse.parse_cse_refs(input_lines[1:])
+    fda_index = self.net.add_v(type='clinicaltrial', label='FDA')
+    fda_refs = refparse.parse_cse_refs(input_lines[1:])
 
-  pm_client.add_pubmed_data(fda_refs)
-  for ref in refs:
-    wos_refs = wos_client.search(_first_author(ref), ref.get('title'), ref.get('journal'), ref.get('year'))
-    if len(wos_refs) == 1:
-      ref.update(wos_refs[0])
+    self.pm_client.add_pubmed_data(fda_refs)
+    for ref in refs:
+      wos_refs = self.wos_client.search(self._first_author(ref), ref.get('title'), ref.get('journal'), ref.get('year'))
+      if len(wos_refs) == 1:
+        ref.update(wos_refs[0])
 
-  for ref in fda_refs:
-    net.add_ref(ref, fda_index)
+    for ref in fda_refs:
+      self.net.add_ref(ref, fda_index)
 
-  wos_client.close()
+    self.wos_client.close()
 
-  output_file_path = drug_name + '.xml'
-  net.layout()
-  net.save(drug_name, output_file_path)
+    output_file_path = drug_name + '.xml'
+    self.net.layout()
+    self.net.save(drug_name, output_file_path)
 
-_main(sys.argv[1])
+td = TopDown()
+td.run(sys.argv[1])
