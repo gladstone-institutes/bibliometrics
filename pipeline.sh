34e5556205db4e9515cf2dfa192479698b7d387e
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2014-09-22 16:01:15 -0700

    Removed linking in pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -7,14 +7,17 @@ if [ "${input_format}" = "pmid" ]; then
   dont_search_trials="--dont-search-trials"
 fi
 
-output_dir="${basename}-${num_levels}-levels-output"
+output_dir="${basename}-levels-${num_levels}-output"
 
 mkdir "${output_dir}"
+
 python src/topdown.py "--format=${input_format}" "--levels=${num_levels}" "${dont_search_trials}" "input/${basename}.txt" "${output_dir}/net"
-python src/xgmml.py "${output_dir}/net.pkl.gz" "${output_dir}/${basename}"
-tar pczf "${output_dir}/${basename}.xgmml.tar.gz" "${output_dir}/${basename}.xgmml"
+
+python src/xgmml.py "${output_dir}/net.pkl.gz" "${output_dir}/net"
+tar pczf "${output_dir}/net.xgmml.tar.gz" "${output_dir}/net.xgmml"
+
 for t in author institution grantagency; do
-  python src/degreesort.py "${output_dir}/net.pkl.gz" "${t}" "${output_dir}/${basename}-${t}"
+  python src/rankings.py "${output_dir}/net.pkl.gz" "${t}" "${output_dir}/${t}"
 done
 
 
